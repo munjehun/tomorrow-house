365c551124c1698a85199acbd8c177e6d0bd0b56
munjehun <miso1489@gmail.com>
2023-05-18 22:07:05 +0900

    🚧 Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -4,9 +4,6 @@ const [orderCtaBookmarkBtn, orderCtaBuyBtn] = orderCta.children
 const orderFormModal = document.querySelector('.order-form-modal')
 const orderOverlay = document.querySelector('.overlay')
 
-console.log(orderCtaBookmarkBtn)
-console.log(orderCtaBuyBtn)
-
 function openOrderFormModal() {
   orderFormModal.classList.add('is-open')
   orderOverlay.classList.add('is-active')
@@ -20,3 +17,20 @@ function closeOrderFormModal() {
 }
 
 orderOverlay.addEventListener('click', closeOrderFormModal)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  let count = parseInt(countSpan.innerHTML.replaceAll(',', ''))
+
+  this.classList.contains('is-active')
+    ? (icon.classList.replace('ic-bookmark-filled', 'ic-bookmark'),
+      (count -= 1))
+    : (icon.classList.replace('ic-bookmark', 'ic-bookmark-filled'),
+      (count += 1))
+
+  countSpan.innerHTML = count.toLocaleString()
+
+  this.classList.toggle('is-active')
+}
+
+orderCtaBookmarkBtn.addEventListener('click', toggleOrderCtaBookmark)
