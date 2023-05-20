19fa37703189b1c9cbb65e6020d87a943eae3e74
munjehun <miso1489@gmail.com>
2023-05-20 20:34:21 +0900

    ♻️ Refactor toggling my-menu

--- js/my-menu.js
+++ js/my-menu.js
@@ -2,22 +2,20 @@ const myMenu = document.querySelector('.my-menu')
 const [myMenuBtn, myMenuModal] = myMenu.children
 
 function toggleMyMenu() {
-  myMenu.classList.toggle('is-active')
-
-  // 최적화를 위해 토글이 됐을때만 closeMyMenuOnClickingOutside() 활성화
-  if (myMenu.classList.contains('is-active')) {
+  // 최적화를 위해 토글이 될 때만 closeMyMenuOnClickingOutside() 활성화
+  if (!myMenu.classList.contains('is-active')) {
     window.addEventListener('click', closeMyMenuOnClickingOutside)
   }
+
+  myMenu.classList.toggle('is-active')
 }
 
 function closeMyMenuOnClickingOutside(e) {
-  console.log('window!!!!!')
-
   // myMenu의 자손중에 e.target이 포함되는지 판단
   if (!myMenu.contains(e.target)) {
     myMenu.classList.remove('is-active')
 
-    // 최적화를 위해 토글이 돼지않으면 closeMyMenuOnClickingOutside() 비활성화
+    // 최적화를 위해 토글이 되지않으면 closeMyMenuOnClickingOutside() 비활성화
     window.removeEventListener('click', closeMyMenuOnClickingOutside)
   }
 }
