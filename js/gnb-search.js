48709e9f4b2bf7ca88a1265324b8a53d6f33b54a
munjehun <miso1489@gmail.com>
2023-05-21 23:57:40 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,18 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryDeleteBtn = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+const gnbSearchHistoryList = gnbSearchHistory.querySelector(
+  '.search-history-list'
+)
 
 function openGnbSearchHistory() {
+  // 리스트가 없으면 is-active 생기지 않도록 먼저 작성
+  if (!gnbSearchHistoryList.children.length) {
+    return
+  }
   gnbSearchHistory.classList.add('is-active')
   window.addEventListener('click', closeSearchHistory)
 }
@@ -14,4 +24,10 @@ function closeSearchHistory(e) {
   }
 }
 
+function deleteAllSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  gnbSearchHistoryList.innerHTML = ''
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+gnbSearchHistoryDeleteBtn.addEventListener('click', deleteAllSearchHistory)
