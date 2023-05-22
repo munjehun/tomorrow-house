f018948e8237c474972334c9970722c80fb654fd
munjehun <miso1489@gmail.com>
2023-05-22 23:22:04 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,12 +1,13 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
-const gnbSearchHistoryDeleteBtn = gnbSearchHistory.querySelector(
+const deleteAllBtn = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 const gnbSearchHistoryList = gnbSearchHistory.querySelector(
   '.search-history-list'
 )
+const deleteBtnList = gnbSearchHistoryList.querySelectorAll('.delete-button')
 
 function openGnbSearchHistory() {
   // 리스트가 없으면 is-active 생기지 않도록 먼저 작성
@@ -14,20 +15,38 @@ function openGnbSearchHistory() {
     return
   }
   gnbSearchHistory.classList.add('is-active')
-  window.addEventListener('click', closeSearchHistory)
+  window.addEventListener('click', closeSearchHistoryOnClickingOutside)
 }
 
-function closeSearchHistory(e) {
+function closeSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeSearchHistoryOnClickingOutside)
+}
+
+function closeSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeSearchHistory)
+    closeSearchHistory()
   }
 }
 
 function deleteAllSearchHistory() {
-  gnbSearchHistory.classList.remove('is-active')
+  closeSearchHistory()
   gnbSearchHistoryList.innerHTML = ''
 }
 
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation() //이벤트 전파 방지 ( closeSearchHistoryOnClickingOutside() 실행 방지 )
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  // 검색기록 1개도 없다면 'is-active' 삭제
+  if (!gnbSearchHistoryList.children.length) {
+    closeSearchHistory()
+  }
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
-gnbSearchHistoryDeleteBtn.addEventListener('click', deleteAllSearchHistory)
+deleteAllBtn.addEventListener('click', deleteAllSearchHistory)
+deleteBtnList.forEach((btn) =>
+  btn.addEventListener('click', deleteSearchHistoryItem)
+)
