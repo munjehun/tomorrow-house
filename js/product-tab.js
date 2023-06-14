27e04befabfd55df0ab60092810f812c11f68e7f
munjehun <miso1489@gmail.com>
2023-06-14 10:58:40 +0900

    ♻️ Refactoring and update IntersectionObserver code : rootMargin

--- js/product-tab.js
+++ js/product-tab.js
@@ -4,6 +4,7 @@ const productTabBtnList = productTab.querySelectorAll('button')
 
 const TOP_HEADER_DESKTOP = 80 + 50 + 54
 const TOP_HEADER_MOBILE = 50 + 40 + 40
+const ORDER_CTA_HEIGHT = 56
 
 let currentActiveTab = productTab.querySelector('.is-active')
 
@@ -58,7 +59,7 @@ const productTabPanelList = productTabIdList.map((panelId) =>
   document.querySelector(`#${panelId}`)
 )
 
-const productTabPanelPositionMap = {}
+// const productTabPanelPositionMap = {}
 
 // 패널 위치 탐지
 // function detectTabPanelPosition() {
@@ -70,62 +71,6 @@ const productTabPanelPositionMap = {}
 
 //   updateActiveTabOnScroll()
 // }
-
-//----------------------스크롤시 탭 활성화 성능개선 (IntersectionObserver)----------------
-// 현재 보여지는 패널에 대한 참거짓 배열
-const visibleProductTabs = productTabIdList.map(() => false)
-
-const options = {
-  root: null,
-  rootMargin: `${
-    window.innerWidth >= 768
-      ? -(TOP_HEADER_DESKTOP + 80)
-      : -(TOP_HEADER_MOBILE + 10)
-  }px 0px -2px 0px`,
-  threshold: [0, 1], //화면에서 보이는 정도가 아니라 요소의 보이는 정도를 나타낸다.
-}
-let observer = new IntersectionObserver(callback, options)
-
-function callback(entries) {
-  entries.forEach((entry) => {
-    const index = productTabIdList.indexOf(`${entry.target.id}`)
-    visibleProductTabs[index] = entry.isIntersecting
-
-    if (disableUpdating) return
-
-    // 마지막 패널을 보고있는지 판단
-    let selectLastOne
-    selectLastOne =
-      index === productTabIdList.length - 1 &&
-      entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99
-
-    // 활성화할 탭 결정 (visibleProductTabs에서 첫 true인 패널의 탭)
-    const tabIndex = selectLastOne
-      ? productTabIdList.length - 1
-      : findFirstIntersecting(visibleProductTabs)
-
-    selectProductTab(tabIndex)
-  })
-}
-
-function findFirstIntersecting(visibleProductTabs) {
-  const index = visibleProductTabs.indexOf(true)
-  return index >= 0 ? index : 0
-}
-
-function selectProductTab(index) {
-  const newActiveTab = productTabItemList[index]
-  currentActiveTab.classList.remove('is-active')
-  currentActiveTab = newActiveTab
-  newActiveTab.classList.add('is-active')
-}
-
-productTabPanelList.forEach((productTabPanel) =>
-  observer.observe(productTabPanel)
-)
-
-//------------------------------------------------------
 
 // 스크롤에 따른 Active Tab
 // function updateActiveTabOnScroll() {
@@ -175,3 +120,57 @@ productTabPanelList.forEach((productTabPanel) =>
 // window.addEventListener('load', detectTabPanelPosition)
 // window.addEventListener('resize', _.throttle(detectTabPanelPosition, 1000))
 // window.addEventListener('scroll', _.throttle(updateActiveTabOnScroll, 200))
+
+//-------------------스크롤시 탭 활성화 성능개선 (IntersectionObserver 버전)-------------------
+
+// 현재 보여지는 패널에 대한 참거짓 배열
+const visibleProductTabs = productTabIdList.map(() => false)
+
+const options = {
+  root: null,
+  rootMargin:
+    window.innerWidth >= 768
+      ? `${-(TOP_HEADER_DESKTOP + 100)}px 0px 0px`
+      : `-30% 0px -${ORDER_CTA_HEIGHT}px 0px`,
+  threshold: [0, 0.3, 0.7, 1], //화면에서 보이는 정도가 아니라 요소의 보이는 정도를 나타낸다.
+}
+let observer = new IntersectionObserver(callback, options)
+
+function callback(entries) {
+  entries.forEach((entry) => {
+    const index = productTabIdList.indexOf(`${entry.target.id}`)
+    visibleProductTabs[index] = entry.isIntersecting
+
+    if (disableUpdating) return
+
+    // 마지막 패널을 보고있는지 판단
+    let selectLastOne
+    selectLastOne =
+      index === productTabIdList.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.99
+
+    // 활성화할 탭 결정 (visibleProductTabs에서 첫 true인 패널의 탭)
+    const tabIndex = selectLastOne
+      ? productTabIdList.length - 1
+      : findFirstIntersecting(visibleProductTabs)
+
+    selectProductTab(tabIndex)
+  })
+}
+
+function findFirstIntersecting(visibleProductTabs) {
+  const index = visibleProductTabs.indexOf(true)
+  return index >= 0 ? index : 0
+}
+
+function selectProductTab(index) {
+  const newActiveTab = productTabItemList[index]
+  currentActiveTab.classList.remove('is-active')
+  currentActiveTab = newActiveTab
+  newActiveTab.classList.add('is-active')
+}
+
+productTabPanelList.forEach((productTabPanel) =>
+  observer.observe(productTabPanel)
+)
